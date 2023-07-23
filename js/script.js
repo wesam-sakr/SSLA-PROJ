ece1d9435d8b708bfacd4b21493d2a7700502dc7
wesam-sakr <wesamsakr801@gmail.com>
2023-07-23 16:51:40 +0300

    bootstrap

--- js/script.js
+++ js/script.js
@@ -12,13 +12,29 @@ else{
 // loader
 $('#loading').fadeOut(500);
 
+$(".play-icon").click(function(){
+    document.querySelector(".vid-wrapper video").play();
+    $(this).addClass("d-none");
+    $(".pause-icon").removeClass("d-none");
+    $('.video-cover-img').slideUp("slow")
+})
+
+$(".pause-icon").click(function(){
+    document.querySelector(".vid-wrapper video").pause();
+    $(this).addClass("d-none");
+    $(".play-icon").removeClass("d-none");
+    $('.video-cover-img').slideDown( "slow" )
+})
+
+
 // owl carousel
 $(document).ready(function(){
+
     $('.partners .owl-carousel').owlCarousel({
         loop:true,
-        margin:10,
+        margin:15,
         responsiveClass:true,
-        rtl:true,
+        rtl:dirAr,
         autoplay:true,
         nav:false,
         dots: false,
@@ -30,7 +46,46 @@ $(document).ready(function(){
                 items:4,
             },
             1000:{
-                items:6,
+                items:5,
+            },
+            function(){
+
+                
+            }
+        }
+    });
+    var owl = $('.partners .owl-carousel');
+    owl.owlCarousel();
+    // Listen to owl events:
+    owl.on('changed.owl.carousel', function(event) {
+        var n = Math.ceil($('.partners .owl-item.active').length/2) ;
+        console.log(n);
+        $('.partners .owl-item.active').each(function (){
+            if(this.hasClass('partner')) {
+                this.classList.remove('partner');
+            }    
+        })
+        
+        var aAitem = $('.partners .owl-item.active')[n]
+        $(aAitem).toggleClass("partner")
+    })
+    $('.article .owl-carousel').owlCarousel({
+        // loop:true,
+        margin:20,
+        // responsiveClass:true,
+        rtl:dirAr,
+        // autoplay:true,
+        nav :true,
+        dots: false,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
             }
         }
     });
@@ -49,7 +104,7 @@ $(document).ready(function(){
     $('.staff .owl-carousel').owlCarousel({
         loop:true,
         margin:20,
-        responsiveClass:true,
+        // responsiveClass:true,
         rtl:dirAr,
         autoplay:true,
         nav:false,
@@ -109,25 +164,30 @@ $('.op-pro-filter').click(function () {
 var dropdown = document.querySelectorAll('.dropdown');
 var dropdownArray = Array.prototype.slice.call(dropdown,0);
 dropdownArray.forEach(function(el){
-	var button = el.querySelector('a[data-toggle="dropdown"]'),
-			menu = el.querySelector('.dropdown-menu'),
-			arrow = button.querySelector('i.icon-arrow');
+    var button = el.querySelector('a[data-toggle="dropdown"]'),
+    menu = el.querySelector('.dropdown-menu'),
+    arrow = button.querySelector('i.icon-arrow');
+    button.onclick = function(event) {
+    $('.dropdown-menu').each( function(){
+    if(!this.hasClass('show')) {
+        this.classList.remove('show');
+    } else this.classList.add('hide');
+    })
 
-	button.onclick = function(event) {
-		if(!menu.hasClass('show')) {
-			menu.classList.add('show');
-			menu.classList.remove('hide');
-			arrow.classList.add('open');
-			arrow.classList.remove('close');
-			event.preventDefault();
-		}
-		else {
-			menu.classList.remove('show');
-			menu.classList.add('hide');
-			arrow.classList.remove('open');
-			arrow.classList.add('close');
-			event.preventDefault();
-		}
+    if(!this.hasClass('show')) {
+        menu.classList.add('show');
+        menu.classList.remove('hide');
+        arrow.classList.add('open');
+        arrow.classList.remove('close');
+        event.preventDefault();
+    }
+    else {
+        menu.classList.remove('show');
+        menu.classList.add('hide');
+        arrow.classList.remove('open');
+        arrow.classList.add('close');
+        event.preventDefault();
+    }
 	};
 })
 Element.prototype.hasClass = function(className) {
